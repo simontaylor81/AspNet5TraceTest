b4f69e0cb17135641e0b07df4aa1fff1f0728169
Simon Taylor <simontaylor81@hotmail.com>
2015-07-26 20:20:59 +0100

    Initial commit

---
+++ src/AspNet5TraceTest/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using System.Diagnostics;
+
+namespace AspNet5TraceTest
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+				Trace.WriteLine("Processing request");
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
